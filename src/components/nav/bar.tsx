abb23ef16b873311cd630eaca0c50d29cbf62c34
Jane Jeon <git@janejeon.com>
2025-03-01 19:02:51 +0900

    sentry works, can remove button

--- src/components/nav/bar.tsx
+++ src/components/nav/bar.tsx
@@ -8,13 +8,6 @@ export function NavBar() {
   return (
     <Flex as="nav" placeItems="center" wrap="wrap" padding={2} bg="bg.muted">
       <NavigationLinks />
-      <button
-        onClick={() => {
-          throw new Error("This is your first error!");
-        }}
-      >
-        Break
-      </button>
       <Spacer />
       <ColorModeButton />
     </Flex>
