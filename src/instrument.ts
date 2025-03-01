952bd1b2ececd75c5f04a99a832c6e9cd1007ad4
Jane Jeon <git@janejeon.com>
2025-03-01 21:17:11 +0900

    try to improve bundle size

--- src/instrument.ts
+++ src/instrument.ts
@@ -1,16 +1,10 @@
-import * as Sentry from "@sentry/react";
+import { init, browserTracingIntegration } from "@sentry/react";
 
-Sentry.init({
+init({
   dsn: import.meta.env.VITE_SENTRY_DSN,
-  integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.replayIntegration(),
-  ],
+  integrations: [browserTracingIntegration()],
   // Tracing
   tracesSampleRate: 1.0, //  Capture 100% of the transactions
   // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
   tracePropagationTargets: ["https://cal.aetherball.com"],
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 });
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import "./instrument.ts";
 
 import "./index.css";
-import "jotai-devtools/styles.css";
+// import "jotai-devtools/styles.css";
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { DevTools } from "jotai-devtools";
+// import { DevTools } from "jotai-devtools";
 import ReactQueryProvider from "./providers/react-query.tsx";
 import { Provider } from "@/components/ui/provider";
 import App from "./App.tsx";
@@ -13,7 +13,7 @@ import App from "./App.tsx";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ReactQueryProvider>
-      <DevTools />
+      {/* <DevTools /> */}
       <Provider>
         <App />
       </Provider>
