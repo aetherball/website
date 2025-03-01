a7aefe6c9b68096b61258e78de0b2e04f2ab3c0c
Jane Jeon <git@janejeon.com>
2025-03-01 19:58:43 +0900

    add bundle analysis

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { analyzer } from "vite-bundle-analyzer";
 import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 // https://vite.dev/config/
@@ -17,9 +18,11 @@ export default defineConfig({
       },
     }),
     tsconfigPaths(),
+    analyzer(),
     sentryVitePlugin({
       org: "janejeon",
       project: "aetherball-frontend",
+      telemetry: false,
     }),
   ],
   build: {
