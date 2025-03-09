14c059fe3795155b63c9a07f9aec8b434a50099d
Jane Jeon <git@janejeon.com>
2025-03-09 22:40:44 +0900

    get sentry to STFU during dev

--- src/instrument.ts
+++ src/instrument.ts
@@ -1,10 +1,12 @@
 import { init, browserTracingIntegration } from "@sentry/react";
 
-init({
-  dsn: import.meta.env.VITE_SENTRY_DSN,
-  integrations: [browserTracingIntegration()],
-  // Tracing
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
-  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ["https://cal.aetherball.com"],
-});
+if (import.meta.env.PROD) {
+  init({
+    dsn: import.meta.env.VITE_SENTRY_DSN,
+    integrations: [browserTracingIntegration()],
+    // Tracing
+    tracesSampleRate: 1.0, //  Capture 100% of the transactions
+    // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+    tracePropagationTargets: ["https://cal.aetherball.com"],
+  });
+}
