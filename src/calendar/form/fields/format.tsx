1a970a01935738c90aed1a609ad7229269b471f7
Jane Jeon <git@janejeon.com>
2025-03-10 01:18:43 +0900

    fix format radiocard + hovercard interactions styling

--- src/calendar/form/fields/format.tsx
+++ src/calendar/form/fields/format.tsx
@@ -1,46 +1,79 @@
 import { useController } from "react-hook-form";
-import { HStack, Fieldset } from "@chakra-ui/react";
-
-import { RadioCardItem, RadioCardRoot } from "@/components/ui/radio-card";
 import {
-  HoverCardArrow,
-  HoverCardContent,
-  HoverCardRoot,
-  HoverCardTrigger,
-} from "@/components/ui/hover-card";
+  HStack,
+  Fieldset,
+  RadioCard,
+  HoverCard,
+  Portal,
+} from "@chakra-ui/react";
+
 import MultilineText from "@/components/text/multiline";
 
 import type { CalendarFormat } from "../schema";
 import type { FieldProps } from "../types";
 
-const formats: {
+type FormatProps = {
   format: CalendarFormat;
   label: string;
   summary: string;
   description: string;
-}[] = [
+};
+
+const formats: FormatProps[] = [
   {
     format: "range",
     label: "Range",
-    summary: "Event spans the calendar",
+    summary: "Event from start to end",
     description: `Each in-game event corresponds to one calendar event, spanning the entire duration of the in-game event.
     Useful if you want to track what in-game events are ongoing at a given time.`,
   },
   {
     format: "point",
     label: "Point",
-    summary: "All-day events for the start and end",
+    summary: "Start & end events",
     description: `Each in-game event corresponds to two calendar events, one for the start and one for the end.
     Useful if you want to track (and complete) in-game events in a timely manner.`,
   },
   {
     format: "point-end",
     label: "Point (End)",
-    summary: "All-day event for the end only",
+    summary: "End events only",
     description: `Each in-game event corresponds to one calendar event, only for the end.
     Useful if you just want to track upcoming deadlines.`,
   },
 ];
+
+function FormatDisplay({ format, label, summary, description }: FormatProps) {
+  return (
+    <HoverCard.Root
+      openDelay={100}
+      closeDelay={100}
+      positioning={{ placement: "top" }} // so that it doesn't get in the way of the button
+    >
+      <RadioCard.Item value={format} width="60">
+        <RadioCard.ItemHiddenInput />
+        <HoverCard.Trigger asChild>
+          <RadioCard.ItemControl>
+            <RadioCard.ItemIndicator />
+            <RadioCard.ItemContent>
+              <RadioCard.ItemText>{label}</RadioCard.ItemText>
+              <RadioCard.ItemDescription>{summary}</RadioCard.ItemDescription>
+            </RadioCard.ItemContent>
+          </RadioCard.ItemControl>
+        </HoverCard.Trigger>
+      </RadioCard.Item>
+
+      <Portal>
+        <HoverCard.Positioner>
+          <HoverCard.Content maxWidth="96">
+            <HoverCard.Arrow />
+            <MultilineText text={description} />
+          </HoverCard.Content>
+        </HoverCard.Positioner>
+      </Portal>
+    </HoverCard.Root>
+  );
+}
 
 export default function FormatField({ control }: FieldProps) {
   const form = useController({
@@ -53,37 +86,19 @@ export default function FormatField({ control }: FieldProps) {
     <Fieldset.Root size="lg">
       <Fieldset.Legend>Event Format</Fieldset.Legend>
 
-      <RadioCardRoot
+      <RadioCard.Root
         value={form.field.value}
         onValueChange={(details) => form.field.onChange(details.value)}
         name={form.field.name}
         marginTop="0"
+        paddingTop="4"
       >
-        <HStack gap="6" align="flex-start">
-          {formats.map(({ format, label, summary, description }) => (
-            <HoverCardRoot
-              key={format}
-              openDelay={100}
-              closeDelay={100}
-              positioning={{ placement: "top" }} // so that it doesn't get in the way of the button
-            >
-              <HoverCardTrigger border="none" asChild>
-                <RadioCardItem
-                  indicatorPlacement="start"
-                  value={format}
-                  label={label}
-                  description={summary}
-                />
-              </HoverCardTrigger>
-
-              <HoverCardContent maxWidth="24rem">
-                <HoverCardArrow />
-                <MultilineText text={description} />
-              </HoverCardContent>
-            </HoverCardRoot>
+        <HStack align="stretch">
+          {formats.map((formatProps) => (
+            <FormatDisplay key={formatProps.format} {...formatProps} />
           ))}
         </HStack>
-      </RadioCardRoot>
+      </RadioCard.Root>
     </Fieldset.Root>
   );
 }
