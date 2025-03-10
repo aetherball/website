b1e6326d0aa01f0ec0a8e21aee348c6f787aa1ea
Jane Jeon <git@janejeon.com>
2025-03-10 18:18:36 +0900

    add meta description automatically

--- src/components/layout/description.tsx
+++ src/components/layout/description.tsx
@@ -1,1 +1,25 @@
-export { Text as Description } from "@chakra-ui/react";
+import { useEffect } from "react";
+
+import { Text } from "@chakra-ui/react";
+
+type Props = {
+  children: string;
+};
+
+export default function Description({ children: description }: Props) {
+  useEffect(() => {
+    let metaTag = document.querySelector<HTMLMetaElement>(
+      "meta[name='description']",
+    );
+
+    if (!metaTag) {
+      metaTag = document.createElement("meta");
+      metaTag.name = "description";
+      document.head.appendChild(metaTag);
+    }
+
+    metaTag.content = description;
+  }, [description]);
+
+  return <Text>{description}</Text>;
+}
