a0e105160da743a2a268bd64bb142704c53d88cd
Jane Jeon <git@janejeon.com>
2025-03-10 13:44:40 +0900

    reduce padding in smaller screen sizes

--- src/components/layout/body.tsx
+++ src/components/layout/body.tsx
@@ -7,8 +7,8 @@ export function Body({ children }: PropsWithChildren<unknown>) {
     <VStack
       as="main"
       width="100%"
-      paddingX={16}
-      paddingY={8}
+      paddingX={{ base: 2, md: 8, lg: 16 }}
+      paddingY={{ base: 2, md: 4, lg: 8 }}
       alignItems="normal"
     >
       {children}
